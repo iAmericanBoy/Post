12182472b4374931ed97cf5fcb5b44e0bbcdf885
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-02-05 13:37:28 -0700

    Adds Post functionality to PostController

--- Controller/Model Controller/PostController.swift
+++ Controller/Model Controller/PostController.swift
@@ -15,6 +15,50 @@ class PostController {
     //sourceOfTruth
     var posts: [Post] = []
     
+    //CRUD
+    //MARK: - Create
+    func addPostWith(username: String, text: String, completion: @escaping () -> Void) {
+        let newPost = Post(text: text, username: username)
+        
+        var postData: Data
+
+
+        do {
+            let encoder = JSONEncoder()
+            postData =  try encoder.encode(newPost)
+        } catch  {
+            print("Error encoding PostData: \(error) \(error.localizedDescription) ")
+            completion()
+            return
+        }
+        
+        guard let url = baseURL else {completion(); return}
+        let postEndpoint = url.appendingPathExtension("json")
+        
+        var request = URLRequest(url: postEndpoint)
+        request.httpBody = postData
+        request.httpMethod = "POST"
+        
+        let dataTask = URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                print("Error posting to API: \(error) \(error.localizedDescription)")
+                completion()
+                return
+            }
+            guard let data = data, let dataString = String(data: data, encoding: .utf8) else {print("error unwrapping data"); completion(); return}
+            
+            print(dataString)
+            
+            self.fetchPosts(completion: {
+                completion()
+            })
+        }
+        dataTask.resume()
+
+  
+    }
+    
+    //MARK: - Read
     func fetchPosts(completion: @escaping () -> Void) {
         guard let url = baseURL else {return}
         
